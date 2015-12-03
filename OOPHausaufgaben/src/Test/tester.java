c543f2cd6142df54aa0cabd75d118b9ebc7c263d
alexander <alexander@Alexander>
2015-12-03 08:29:11 +0100

    lol

--- OOPHausaufgaben/src/Test/tester.java
+++ OOPHausaufgaben/src/Test/tester.java
@@ -3,6 +3,6 @@ package Test;
 public class tester {
 	public static void main(String[] args) {
 		System.out.println("Dies ist ein Test!!!111");
-		System.out.println("hallo");
+		System.out.println("hallolol");
 	}
 }
